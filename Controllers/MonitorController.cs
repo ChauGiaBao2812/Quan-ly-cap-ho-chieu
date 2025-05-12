feb7ef3e5c1bbca5eba42d5ce41a9c3986a7b866
Le Tuan Dat <dat261303@gmail.com>
2025-05-12 22:02:33 +0700

    Add MonitorController.cs,Models/HoSo,cs,Views/Monitor (Detail, ListProfile)

---
+++ Controllers/MonitorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class MonitorController : Controller
+    {
+        public IActionResult ListProfile()
+        {
+            var danhSachHoSo = new List<HoSo>
+            {
+                new HoSo { MaHoSo = "HS01", TinhTrang = "Lưu trữ", DaDuyet = true, NgayNop = new DateTime(2025, 3, 12) },
+                new HoSo { MaHoSo = "HS02", TinhTrang = "Xác thực", DaDuyet = true, NgayNop = new DateTime(2025, 3, 11) },
+                new HoSo { MaHoSo = "HS03", TinhTrang = "Xét duyệt", DaDuyet = false, NgayNop = new DateTime(2025, 3, 10) },
+                new HoSo { MaHoSo = "HS04", TinhTrang = "Lưu trữ", DaDuyet = false, NgayNop = new DateTime(2025, 3, 9) }
+            };
+            return View(danhSachHoSo);
+        }
+
+        public IActionResult Detail(string id)
+        {
+            // Dữ liệu mẫu - nên thay bằng lấy từ database
+            var hoSo = new HoSo
+            {
+                MaHoSo = id,
+                TinhTrang = "Xác thực",
+                DaDuyet = true,
+                NgayNop = new DateTime(2025, 3, 12),
+                GhiChuXacThuc = "Đã xác thực thông tin người nộp",
+                GhiChuXetDuyet = "Đủ điều kiện xét duyệt",
+                IdUserXacThuc = "user_XT_001",
+                IdUserXetDuyet = "user_XD_002",
+                IdUserLuuTru = "user_LT_003"
+            };
+
+            return View(hoSo);
+        }
+    }
+}
